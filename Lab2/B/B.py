5c219aae8b5b1df6d225204a8cda46a70a64b567
Sergey Ivanov <swordie.ser@gmail.com>
2021-03-11 12:09:35 +0300

    Done B

--- Lab2/B/B.py
+++ Lab2/B/B.py
@@ -7,42 +7,49 @@ setrecursionlimit(10 ** 9)
 
 def main():
     def dfs(vertex):
-        nonlocal cycle, path
-        path.append(vertex)
-        color[vertex] = 1
-        for u in graph[vertex]:
-            if color[u] == 0:
-                dfs(u)
-            if color[u] == 1:
-                cycle = True
-        color[vertex] = 2
+        nonlocal cycle_start
+        graph[vertex][0] = 1
+        path.append(vertex + 1)
+        for i in range(1, len(graph[vertex])):
+            next_vertex = graph[vertex][i]
+            if graph[next_vertex][0] == 0:
+                if dfs(graph[vertex][i]):
+                    stack.append(vertex + 1)
+                    return True
+            elif graph[next_vertex][0] == 1:
+                stack.append(vertex + 1)
+                cycle_start = next_vertex + 1
+                return True
+        graph[vertex][0] = 2
+        path.pop()
 
     with open("cycle.in") as f:
         n, m = list(map(int, f.readline().split()))
-        graph = [[] for _ in range(n)]
+        graph = [[0] for _ in range(n)]
+        cycle_start = None
+
         for i in range(m):
             a, b = list(map(int, f.readline().split()))
             graph[a - 1].append(b - 1)
 
-    color = [0 for _ in range(n)]  # 0 - white, 1 - grey, 2 - black
-    cycle = False
     path = []
-    for i in range(n):
-        if color[i] == 0:
-            path = []
-            dfs(i)
-            if cycle:
-                break
-
-    path.append(path[0])
-    del path[0]
+    stack = []
+    final = []
 
     with open("cycle.out", "w") as f:
-        if cycle:
-            f.write("YES\n")
-            for v in path:
-                f.write(str(v+1) + " ")
-
+        for i in range(len(graph)):
+            if graph[i][0] == 0:
+                if dfs(i):
+                    f.write("YES\n")
+                    for elem in stack:
+                        if elem != cycle_start:
+                            final.append(elem)
+                        else:
+                            final.append(elem)
+                            break
+                    for i in range(path.index(cycle_start), len(path)):
+                        f.write(str(path[i]) + " ")
+                    break
         else:
             f.write("NO")
 
